f987e535a3623be9e7b63c3ccd93c63eadb83b63
Documenter.jl <documenter@juliadocs.github.io>
2023-05-11 12:10:33 +0000

    build based on fb5d31a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MatricesForHomalg","category":"page"},{"location":"#MatricesForHomalg","page":"Home","title":"MatricesForHomalg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MatricesForHomalg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MatricesForHomalg]","category":"page"}]
+}
